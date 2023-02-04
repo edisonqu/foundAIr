0e6f791856f9b4e3854cafae5a2ec9eda9777e45
stephen-ics <stephenni1234@gmail.com>
2023-02-04 16:00:47 -0500

    typeform effect

---
+++ client/src/components/Confirm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Confirm = () => {
+  return (
+    <div>Confirm</div>
+  )
+}
+
+export default Confirm
---
+++ client/src/components/Form2.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import { motion } from "framer-motion";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: "8rem auto 5rem",
+      maxWidth: "60%"
+    }
+  }
+}));
+
+const Form2 = () => {
+  const classes = useStyles();
+  return (
+    <div>
+      <form className={classes.root} autoComplete="off">
+        <motion.div
+          className="col-md-6 offset-md-3"
+          initial={{ y: "50vh" }}
+          animate={{ y: 0 }}
+          transition={{ stiffness: 150 }}
+        >
+          <TextField id="email-id" fullWidth label="Email id" />
+        </motion.div>
+    </form>
+    </div>
+  )
+}
+
+export default Form2
