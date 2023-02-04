5fd3a9367a90cbb439b34e88e94dfaec848549b2
Sam Hawkens <ian.korovinsky@gmail.com>
2023-02-04 18:48:02 -0500

    Simplified api calls algo

--- server/api_call.py
+++ server/api_call.py
@@ -11,7 +11,22 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 summary = "null"
 
-def get_executive_summary(bname, idea, budget):
+prompts = [f"the business name is {bname}. i am writing a business plan. business idea: {idea}. minimum 600 words. describe the problem faced by people without my business idea, the pain points my business idea targets, and explain my idea. no headings, no jot notes.", f"the business name is {bname}. i am writing a business plan. business idea: {idea}. minimum 500 words. write me a market analysis, explaining the target market, the customer segments, explain how the consumers will take advantage of this, and explain the competition from other business i will face. no headings and no jot notes.", f"the business name is {bname}. i am writing a business plan. business idea: {idea}. minimum 500 words. write me the unique value proposition of my idea and the competitive advantage of my idea. no headings and no jot notes.", f"the business name is {bname}. i am writing a business plan. business idea: {idea}. minimum 550 words. explain the marketing channels i can take advantage of to promote my idea and interact with my consumers, both online and in person. explain how i can sell my idea in person and online. no headings and no jot notes.", f"the business name is {bname}. i am writing a business plan. write a timeline to launch my business with numbers. minimum 500 words. business idea: {idea}. no headings and no jot notes.", f"the business name is {bname}.i am writing a business plan. business idea: {idea}. my budget is only {budget}. give me a budget for my business and include descriptions for each cost. include numbers and jot form.", f"the business name is {bname}. i am writing a business plan. business idea: {idea}. minimum 500 words. explain the key metrics with specific numbers in jot form. describe methods to reduce risk. explain how we will reassess and reevaluate our progress. no headings.", f"summarize the following text in 150 words. {summary}"]
+
+
+def call_api(bname, idea, budget, section):
+  response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=prompts[section],
+    temperature=0.1,
+    max_tokens=3500,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+  )
+  return response
+
+def get_executive_summary(bname, idea, budget, section):
   response = openai.Completion.create(
     model="text-davinci-003",
     prompt=f"the business name is {bname}. i am writing a business plan. write an executive summary for my business. minimum 500 words. business idea: {idea}. topics to cover: problem, solution, customer segments, financials on a {budget} budget, marketing channels both online and in-person, sales methods both online and in-person, key metrics, risk reduction, and competitive advantage. section paragraphs by topic. no headings and no jot notes.",
@@ -23,99 +38,3 @@ def get_executive_summary(bname, idea, budget):
   )
   summary = response["choices"][0]['text']
   return summary
-
-def get_business_overview(bname, idea):
-  response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=f"the business name is {bname}. i am writing a business plan. business idea: {idea}. minimum 600 words. describe the problem faced by people without my business idea, the pain points my business idea targets, and explain my idea. no headings, no jot notes.",
-    temperature=0.1,
-    max_tokens=3500,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
-  )
-  return response["choices"][0]['text']
-
-def get_market_analysis(bname, idea):
-  response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt="the business name is {bname}. i am writing a business plan. business idea: {idea}. minimum 500 words. write me a market analysis, explaining the target market, the customer segments, explain how the consumers will take advantage of this, and explain the competition from other business i will face. no headings and no jot notes.",
-    temperature=0.1,
-    max_tokens=3500,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
-  )
-  return response["choices"][0]['text']
-
-def get_unique_value_proposition(bname, idea):
-  response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=f"the business name is {bname}. i am writing a business plan. business idea: {idea}. minimum 500 words. write me the unique value proposition of my idea and the competitive advantage of my idea. no headings and no jot notes.",
-    temperature=0.1,
-    max_tokens=3500,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
-  )
-  return response["choices"][0]['text']
-
-def get_sales_and_marketing_plan(bname, idea):
-  response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=f"the business name is {bname}. i am writing a business plan. business idea: {idea}. minimum 550 words. explain the marketing channels i can take advantage of to promote my idea and interact with my consumers, both online and in person. explain how i can sell my idea in person and online. no headings and no jot notes.",
-    temperature=0.1,
-    max_tokens=3500,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
-  )
-  return response["choices"][0]['text']
-
-def get_timeline(bname, idea):
-  response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=f"the business name is {bname}. i am writing a business plan. write a timeline to launch my business with numbers. minimum 500 words. business idea: {idea}. no headings and no jot notes.",
-    temperature=0.1,
-    max_tokens=3500,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
-  )
-  return response["choices"][0]['text']
-
-def get_financials(bname, idea, budget):
-  response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=f"the business name is {bname}.i am writing a business plan. business idea: {idea}. my budget is only {budget}. give me a budget for my business and include descriptions for each cost. include numbers and jot form.",
-    temperature=0.1,
-    max_tokens=3500,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
-  )
-  return response["choices"][0]['text']
-
-def get_metrics_and_risk_reduction(bname, idea):
-  response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=f"the business name is {bname}. i am writing a business plan. business idea: {idea}. minimum 500 words. explain the key metrics with specific numbers in jot form. describe methods to reduce risk. explain how we will reassess and reevaluate our progress. no headings.",
-    temperature=0.1,
-    max_tokens=3500,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
-  )
-  return response["choices"][0]['text']
-
-def get_conclusion():
-  response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=f"summarize the following text in 150 words. {summary}",
-    temperature=0.1,
-    max_tokens=3500,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
-  )
-  return response["choices"][0]['text']
