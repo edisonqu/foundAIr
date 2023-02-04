7803f189ce44e3fce92fb0af52cd81bd2e1f3aff
stephen-ics <stephenni1234@gmail.com>
2023-02-04 14:34:55 -0500

    navbar works

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import React, { Suspense } from "react";
+import { AnimatePresence } from 'framer-motion'
+ 
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Plan from './pages/Plan'
 
-const App = () => {
+import './App.css';
+
+
+function App() {
   return (
-    <div>App</div>
-  )
+    <div className="App bg-white">
+    <BrowserRouter>
+        <Suspense fallback={<div>Page Loading...</div>}>
+        <Navbar />
+        <AnimatePresence
+         mode='wait'>
+           <Routes>
+             <Route path="/" exact element={<Home />} />
+             <Route path="/plan" exact element={<Plan />} />
+           </Routes>
+        </AnimatePresence>
+        </Suspense>
+    </BrowserRouter>
+  </div>
+  );
 }
 
-export default App
+export default App;
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,96 @@
-import React from 'react'
+import { wrap } from "framer-motion";
+import { useEffect, useState } from "react"
 
+
+// import Logo from "../public/logo-transparent.png";
+ 
+import { GoThreeBars } from "react-icons/go";
+
+import {
+   Link,
+   useLocation
+} from "react-router-dom";
+
+
+const links = [
+    {
+        name: "Home",
+        link: "/",
+        id: "home",
+        priority: false,
+        transparant: true,
+    },
+    {
+        name: "Plan",
+        link: "/plan",
+        id: "plan",
+        priority: false,
+        transparant: true,
+    }
+
+]
+
+   /*
+   {
+       name: "Sign Up",
+       link: "/sign-up",
+       id: "call-to-action",
+       priority: true
+   },
+   */
+
+ 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+   const [showDropdown, setShowDropdown] = useState(false);
+   const { pathname } = useLocation();
 
-export default Navbar
+   const [loggedIn, setLoggedIn] = useState(false)
+
+   const handleClick = () => {
+       setLoggedIn(!loggedIn)
+   }
+
+   return (
+    //{pathname ? opacity:0 : opacity:1}
+       <header className="border-[#130611] border-transparent border-solid border-2 flex flex-col justify-center bg-[#130611] bg-opacity-0 z-[99999999] min-h-[7vh] py-2 lg:py-4">
+           <div className="container px-4 mx-auto lg:flex lg:items-center m-30">
+               <div className="flex justify-between items-center">
+                   <Link className="flex flex-row items-center gap-4 font-bold text-2xl text-teal" to="/">
+                       {/* <img src="" alt="Logo" width={29} height={42} /> */}
+                       <h2 className="text-4xl text-black font-mono hover:text-yellow-900">MacHacks </h2>
+                   </Link>
+ 
+                   <button
+                       className="border border-solid border-gray-200 px-3 py-1 rounded text-gray-200 opacity-50 hover:opacity-75 lg:hidden cursor-pointer"
+                       aria-label="Menu"
+                       data-test-id="navbar-menu"
+                       onClick={
+                           () => {
+                               setShowDropdown(!showDropdown);
+                           }}
+                   >
+                       <GoThreeBars />
+                   </button>
+               </div>
+ 
+               <div className={`${showDropdown ? "flex" : "hidden"} lg:flex flex-col lg:flex-row lg:ml-auto mt-3 lg:mt-0`} data-test-id="navbar">
+                   
+
+                        <div>
+                            {links.map(({ name, link, priority, id }) => 
+                                <Link key={name} className={`${priority ? "text-purple-900 hover:bg-purple-900 hover:text-white text-center border border-solid border-purple-900 mt-1 lg:mt-0 lg:ml-1" : "text-black text-2xl hover:bg-gray-200/25 hover:text-yellow-900"} p-2 lg:px-4 lg:mx-2 rounded duration-300 transition-colors ${pathname === name && "font-bold"}`} to={link}>
+                                    {name}
+                                </Link>
+                            )}
+
+                        </div>
+  
+         
+               </div>
+               
+           </div>
+       </header>
+   )
+}
+ 
+export default Navbar;
