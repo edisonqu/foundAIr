ddf96a6dd3bbf201c30cf4df728c81f34d87b40b
edisonqu <edisonquu@gmail.com>
2023-02-06 15:03:14 -0500

    Shortened the PDF and OpenAI production time by 7x by using aiohttp

--- server/create_pdf.py
+++ server/create_pdf.py
@@ -1,11 +1,11 @@
 from fpdf import FPDF
 from api_call import *
 
-title = 'Business Plan'
-company_name = "Edison's Dog Store"
-author = "Edison Qu"
-idea = "dog plushies"
-budget = 3000
+# title = 'Business Plan'
+# company_name = "Edison's Dog Store"
+# author = "Edison Qu"
+# idea = "dog plushies"
+# budget = 3000
 # prompts = [f"the business name is {company_name}. i am writing a business plan. business idea: {idea}. minimum 600 words. describe the problem faced by people without my business idea, the pain points my business idea targets, and explain my idea. no headings, no jot notes.", f"the business name is {company_name}. i am writing a business plan. business idea: {idea}. minimum 500 words. write me a market analysis, explaining the target market, the customer segments, explain how the consumers will take advantage of this, and explain the competition from other business i will face. no headings and no jot notes.", f"the business name is {company_name}. i am writing a business plan. business idea: {idea}. minimum 500 words. write me the unique value proposition of my idea and the competitive advantage of my idea. no headings and no jot notes.", f"the business name is {company_name}. i am writing a business plan. business idea: {idea}. minimum 550 words. explain the marketing channels i can take advantage of to promote my idea and interact with my consumers, both online and in person. explain how i can sell my idea in person and online. no headings and no jot notes.", f"the business name is {company_name}. i am writing a business plan. write a timeline to launch my business with numbers. minimum 500 words. business idea: {idea}. no headings and no jot notes.", f"the business name is {company_name}.i am writing a business plan. business idea: {idea}. my budget is only {budget}. give me a budget for my business and include descriptions for each cost. include numbers and jot form.", f"the business name is {company_name}. i am writing a business plan. business idea: {idea}. minimum 500 words. explain the key metrics with specific numbers in jot form. describe methods to reduce risk. explain how we will reassess and reevaluate our progress. no headings.", f"summarize the following text in 150 words. {summary}"]
 
 def create_pdf(title,company_name,author,idea,budget):
@@ -64,7 +64,9 @@ def create_pdf(title,company_name,author,idea,budget):
             self.set_font('Arial', "B", 24)
             self.cell(0, 12, title, 0, 1, 'C', False)
             self.set_font('Arial', "", 14)
-            self.multi_cell(0, 5, output.replace("’","'"))
+            fix_unicode = output.replace ("’","'")
+            final_output = fix_unicode.replace("•",'- ')
+            self.multi_cell(0, 5, final_output.replace("’","'"))
 
     pdf = PDF(format="A4")
     pdf.set_title(title)
@@ -72,18 +74,14 @@ def create_pdf(title,company_name,author,idea,budget):
     pdf.title_page(company_name,author)
     pdf.table_of_contents()
     pdf.set_font('Arial',"",24)
-    # topics = ["Business Overview","Market Analysis","Competitive Advantage","Sales & Market Strategy","Timeline","Finance","Key Metrics and Risk Deduction","Conclusion"]
-    # summary = get_executive_summary(company_name,idea,budget)
-    # pdf.body_page("Executive Summary",summary)
-    # prompts = [f"the business name is {company_name}. i am writing a business plan. business idea: {idea}. minimum 600 words. describe the problem faced by people without my business idea, the pain points my business idea targets, and explain my idea. no headings, no jot notes.", f"the business name is {company_name}. i am writing a business plan. business idea: {idea}. minimum 500 words. write me a market analysis, explaining the target market, the customer segments, explain how the consumers will take advantage of this, and explain the competition from other business i will face. no headings and no jot notes.", f"the business name is {company_name}. i am writing a business plan. business idea: {idea}. minimum 500 words. write me the unique value proposition of my idea and the competitive advantage of my idea. no headings and no jot notes.", f"the business name is {company_name}. i am writing a business plan. business idea: {idea}. minimum 550 words. explain the marketing channels i can take advantage of to promote my idea and interact with my consumers, both online and in person. explain how i can sell my idea in person and online. no headings and no jot notes.", f"the business name is {company_name}. i am writing a business plan. write a timeline to launch my business with numbers. minimum 500 words. business idea: {idea}. no headings and no jot notes.", f"the business name is {company_name}.i am writing a business plan. business idea: {idea}. my budget is only {budget}. give me a budget for my business and include descriptions for each cost. include numbers and jot form.", f"the business name is {company_name}. i am writing a business plan. business idea: {idea}. minimum 500 words. explain the key metrics with specific numbers in jot form. describe methods to reduce risk. explain how we will reassess and reevaluate our progress. no headings.", f"summarize the following text in 150 words. Make it a conclusion for a business plan. {summary}"]
-    # #
-    # summaries = ''
-    # for index_number in range(len(topics)):
-    #     if "summarize the following text in 150 words" in prompts[index_number]:
-    #         print('used')
-    #         pdf.body_page(topics[index_number],call_api(prompts[index_number],summary))
-    #     else:
-    #         pdf.body_page(topics[index_number], call_api(prompts[index_number], summaries))
+    topics = ["Executive Summary", "Business Overview", "Market Analysis", "Competitive Advantage",
+              "Sales & Market Strategy", "Timeline", "Finance", "Key Metrics and Risk Deduction", "Conclusion"]
+
+    # Removed support for an actual summary
+
+    AI_answers = call_api(company_name,idea,budget)
+    for index_number in range(len(topics)):
+        pdf.body_page(topics[index_number], AI_answers[index_number]['choices'][0]['text'])
     pdf.output('tmp/business_plan.pdf', 'F')
     return "solid"
 
